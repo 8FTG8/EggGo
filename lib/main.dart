3ae63d9e5d7954e9d4cf0210f2d9751e80b11e7f
Felipe T. Granvile <felipegranvile01246@gmail.com>
2025-09-22 18:53:16 -0300

    feat: adiciona a inicialização dos serviços essenciais na inicialização do app (Firebase, SQFlite).

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-//import 'package:flutter/foundation.dart';
 
-import 'core/firebase/firebase_service.dart';
+import 'a_core/firebase/firebase_service.dart';
+import 'f_services/local_sql_service.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await LocalStorageService.instance.database;
   FirebaseAuth.instance.setLanguageCode('pt');
-  
+
   runApp(
     DevicePreview(
-      enabled: false, //!kReleaseMode,
+      enabled: !kReleaseMode,
       builder: (context) => const MyApp(),
     ),
   );
